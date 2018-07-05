095c36d73f4716edb6773559af381cb7cc36280a
Alexey Kugachev <alexey_kugachev@epam.com>
2018-07-05 13:34:52 +0300

    добавлено задание 3

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -10,6 +10,28 @@ namespace Task03
     {
         static void Main(string[] args)
         {
+            string[][] jaggedArray = new string[3][];
+            jaggedArray[0] = new string[3] { "Объект сравнения\\культура", "ru", "en" };
+            jaggedArray[1] = new string[3] { "Формат отображения даты и времени", "ru", "en" };
+            jaggedArray[2] = new string[2] { "Формат отображения числовых данных ", "ru" };
+
+            string[][] jaggedArray2 = new string[3][];
+            jaggedArray2[0] = new string[3] { "Объект сравнения\\культура", "ru", "invariant" };
+            jaggedArray2[1] = new string[2] { "Формат отображения даты и времени", "ru" };
+            jaggedArray2[2] = new string[2] { "Формат отображения числовых данных ", "ru" };
+
+            Console.WriteLine("Номер таблицы: \n1)ru+en\n2)ru+inv");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "2":
+                    TableBuilder.BuildByJaggedArr(jaggedArray2);
+                    break;
+                default:
+                    TableBuilder.BuildByJaggedArr(jaggedArray);
+                    break;
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ Task03/TableBuilder.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+    class TableBuilder
+    {
+        private const int defaultLength = 40;
+        public static void BuildBy2DArr<T>(T[,] arr)
+        {
+            int maxArrLength = arr.GetLength(0);
+            int maxElementLength = GetMaxElement(arr);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                sb.AppendFormat("{0}\n", new String('-', maxArrLength * maxElementLength + maxArrLength));
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    try
+                    {
+                        sb.AppendFormat("|{0," + maxElementLength + "}|", IfOutCut(arr[i, j]));
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        sb.AppendFormat("|{0," + maxElementLength + "}|", "-");
+                    }
+                }
+                sb.AppendFormat("\n{0}\n", new String('-', maxArrLength * maxElementLength + maxArrLength));
+            }
+            Console.Write(sb);
+        }
+
+        public static void BuildByJaggedArr<T>(T[][] arr)
+        {
+            int maxArrLength = GetMaxRange(arr);
+            int maxElementLength = GetMaxElement(arr);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                sb.AppendFormat("{0}\n", new String('-', maxArrLength * maxElementLength + maxArrLength * 2));
+                for (int j = 0; j < maxArrLength; j++)
+                {
+                    try
+                    {
+                        sb.AppendFormat("|{0," + maxElementLength + "}|", IfOutCut(arr[i][j]));
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        sb.AppendFormat("|{0," + maxElementLength + "}|", "-");
+                    }
+                }
+                sb.AppendFormat("\n{0}\n", new String('-', maxArrLength * maxElementLength + maxArrLength * 2));
+            }
+            Console.Write(sb);
+        }
+
+        public static int GetMaxRange<T>(T[][] arr)
+        {
+            int maxLength = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                if (arr[i].Length > maxLength)
+                {
+                    maxLength = arr[i].Length;
+                }
+            }
+            return maxLength;
+        }
+
+        public static int GetMaxElement<T>(T[][] arr)
+        {
+            int maxLength = 0;
+            foreach (T[] t in arr)
+                foreach (T k in t)
+                    if (k.ToString().Length > maxLength)
+                    {
+                        maxLength = k.ToString().Length;
+                    }
+            return maxLength > defaultLength ? defaultLength : maxLength;
+        }
+
+        public static int GetMaxElement<T>(T[,] arr)
+        {
+            int maxLength = 0;
+            foreach (T t in arr)
+                if (t.ToString().Length > maxLength)
+                {
+                    maxLength = t.ToString().Length;
+                }
+            return maxLength;
+        }
+
+        public static string IfOutCut<T>(T t)
+        {
+            return IfOutCut(t, defaultLength);
+        }
+
+        public static string IfOutCut<T>(T t, int lineLength)
+        {
+            string str = t.ToString();
+            if (str.Length >= lineLength)
+            {
+                return str.Substring(0, defaultLength - 3) + "...";
+            }
+            else
+            {
+                return str;
+            }
+        }
+    }
+}
